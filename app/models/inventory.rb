d9bbcd7ce40c3f439c9c13ffac1216eaf6ece171
Nathalie Ravilet <natz.ravilet@gmail.com>
2016-04-08 20:52:06 -0300

    railroady diagram generated

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,5 @@
 class Inventory < ActiveRecord::Base
 	validates_uniqueness_of :serial_number, presence: true 
+	has_many :operations,  dependent: :destroy
+	has_many :workers
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,5 +1,6 @@
 class Worker < ActiveRecord::Base
 	validates :name, presence: true 
     belongs_to :inventory 
+    has_many :operations,  dependent: :destroy
 
 end
