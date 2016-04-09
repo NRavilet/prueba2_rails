8982618b9814409cb07b4be70b4b59bfc2df3892
Nathalie Ravilet <natz.ravilet@gmail.com>
2016-04-08 21:55:29 -0300

    last commit

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,6 +2,7 @@ class Operation < ActiveRecord::Base
   belongs_to :workers
   has_one :inventory
   
-  scope :latest_operations, -> { where(Operation.last(5)) }
+  scope :latest_operations, -> { last(5) }
+  scope :last_day, -> { where('created_at  >= #{Time.now - 1.days}') }
 
 end
